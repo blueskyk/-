0b3dc4f50d193808cdd6c0ae1ccd9a496c11498b
blueskyk <1419293032@qq.com>
2021-01-13 16:42:56 +0800

    话题介绍组件封装

---
+++ 糗事百科/components/topic/topic-info.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="topic-info">
+		<view class="topic-info-t u-f-ac">
+			<image :src="topicInfo.titlePic" mode="widthFix" lazy-load></image>
+			<view class="info-title">#{{topicInfo.title}}#</view>
+		</view>
+		<view class="topic-info-c u-f-ac">
+			<view>动态 {{topicInfo.totalNmu}}</view>
+			<view>今日 {{topicInfo.todayNmu}}</view>
+		</view>
+		<view class="topic-info-b">{{topicInfo.desc}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			topicInfo: Object
+		}
+	}
+</script>
+
+<style lang="scss">
+	.topic-info {
+		padding: 0 25rpx;
+		.topic-info-t {
+			position: relative;
+			image {
+				position: absolute;
+				top: -75rpx;
+				width: 150rpx;
+				height: 150rpx;
+				border-radius: 20rpx;
+			}
+			.info-title {
+				margin-left: 170rpx;
+				flex: 1;
+				font-size: 35rpx;
+			}
+		}
+		.topic-info-c {
+			padding: 35rpx 0 15rpx;
+			color: #cdcdcd;
+		}
+		.topic-info-b {
+			color: #a3a3a3;
+			font-size: 32rpx;
+			padding-bottom: 10rpx;
+		}
+		
+	}
+</style>
--- 糗事百科/pages/topic-detail/topic-detail.vue
+++ 糗事百科/pages/topic-detail/topic-detail.vue
@@ -1,22 +1,49 @@
 <template>
 	<view>
-		我是话题详情页
+		<!-- 背景 -->
+		<view class="topic-bg">
+			<image src="../../static/demo/topicpic/13.jpeg" mode="aspectFill"></image>
+		</view>
+		<!-- 话题信息 -->
+			<topic-info :topicInfo="topicInfo"></topic-info>
 	</view>
 </template>
 
 <script>
+	import topicInfo from "../../components/topic/topic-info.vue"
 	export default {
 		data() {
 			return {
-				
+				topicInfo: {
+					titlePic: "../../static/demo/topicpic/13.jpeg",
+					title: "忆往事,敬余生",
+					desc: "我是描述",
+					totalNmu: "1000",
+					todayNmu: "1000"
+				}
 			}
 		},
 		methods: {
 			
+		},
+		components: {
+			topicInfo
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+.topic-bg {
+	position: relative;
+	width: 100%;
+	height: 300rpx;
+	overflow: hidden;
+	image {
+		width: 100%;
+		position: absolute;
+		filter: blur(20rpx);
+	}
+}
+
 
 </style>
