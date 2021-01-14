293dd2fde35acd0797c4a99bffe892b486c5225e
blueskyk <1419293032@qq.com>
2021-01-14 19:55:41 +0800

    小纸条聊天列表组件封装

---
+++ 糗事百科/components/paper/paper-list.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="paper-list u-f-ac">
+		<image :src="item.userPic" mode="widthFix"></image>
+		<view class="user-box">
+			<view class="u-f-ac u-f-jsb user-box">
+				<view class="name">{{item.userName}}</view>
+				<view class="time">{{item.time}}</view>
+			</view>
+			<view class="user-info u-f-ac u-f-jsb">
+				{{item.info}}
+				<template v-if="item.noReadNum > 0">
+					<uni-badge :text="item.noReadNum" type="error"></uni-badge>
+				</template>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniBadge from '../uni-badge/uni-badge.vue';
+	export default {
+		props: {
+			item: Object,
+			index: Number
+		},
+		components: {
+			uniBadge
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.paper-list {
+		padding: 20rpx 0;
+		border-bottom: 1rpx solid #eee;
+		image {
+			width: 100rpx;
+			height: 100rpx;
+			border-radius: 50%;
+			margin-right: 20rpx;
+		}
+		.user-box {
+			flex: 1;
+			.name {
+				font-size: 35rpx;
+			}
+			.time {
+				color: #cbcbcb;
+			}
+		}
+		.user-info {
+			color: #999;
+		}
+	}
+</style>
--- 糗事百科/pages/paper/paper.vue
+++ 糗事百科/pages/paper/paper.vue
@@ -1,22 +1,83 @@
 <template>
 	<view>
-		小纸条页面
+		<view class="paper-box">
+			<!-- 小纸条列表 -->
+			<block v-for="(item,index) in list" :key="index">
+				<paper-list :item="item" :index="index"></paper-list>
+			</block>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
+import paperList from "../../components/paper/paper-list.vue";
+export default {
+	components: {
+		paperList
+	},
+	data() {
+		return {
+			list: [
+				{
+					userPic: "../../static/demo/userpic/6.jpg",
+					userName: "我是昵称",
+					time: "11:07",
+					info: "我是信息",
+					noReadNum: "17"
+				},
+				{
+					userPic: "../../static/demo/userpic/6.jpg",
+					userName: "我是昵称",
+					time: "11:07",
+					info: "我是信息",
+					noReadNum: "0"
+				},
+				{
+					userPic: "../../static/demo/userpic/6.jpg",
+					userName: "我是昵称",
+					time: "11:07",
+					info: "我是信息",
+					noReadNum: "0"
+				},
+				{
+					userPic: "../../static/demo/userpic/6.jpg",
+					userName: "我是昵称",
+					time: "11:07",
+					info: "我是信息",
+					noReadNum: "17"
+				},
+				{
+					userPic: "../../static/demo/userpic/6.jpg",
+					userName: "我是昵称",
+					time: "11:07",
+					info: "我是信息",
+					noReadNum: "17"
+				},
 				
-			}
-		},
-		methods: {
-			
-		}
-	}
+				{
+					userPic: "../../static/demo/userpic/6.jpg",
+					userName: "我是昵称",
+					time: "11:07",
+					info: "我是信息",
+					noReadNum: "17"
+				},
+				{
+					userPic: "../../static/demo/userpic/6.jpg",
+					userName: "我是昵称",
+					time: "11:07",
+					info: "我是信息",
+					noReadNum: "17"
+				}
+				
+			]
+		};
+	},
+	methods: {}
+};
 </script>
 
-<style>
-
+<style lang="scss">
+.paper-box {
+	padding: 0 20rpx;
+}
 </style>
