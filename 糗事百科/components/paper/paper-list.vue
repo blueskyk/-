c6bfc5b092217d93b43033d7a798e089e3ee20db
blueskyk <1419293032@qq.com>
2021-01-16 13:25:41 +0800

    聊天页面的完善

--- 糗事百科/components/paper/paper-list.vue
+++ 糗事百科/components/paper/paper-list.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="paper-list u-f-ac animated fadeInLeft fast">
+	<view class="paper-list u-f-ac animated fadeInLeft fast" @tap="openDetail">
 		<image :src="item.userPic" mode="widthFix"></image>
 		<view class="user-box">
 			<view class="u-f-ac u-f-jsb user-box">
@@ -25,6 +25,13 @@
 		},
 		components: {
 			uniBadge
+		},
+		methods: {
+			openDetail() {
+				uni.navigateTo({
+					url: "../../pages/user-chat/user-chat"
+				})
+			}
 		}
 	}
 </script>
