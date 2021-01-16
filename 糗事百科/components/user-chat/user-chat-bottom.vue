cde2ba4c03794ffdc64bbbbcebcd984377a87ac5
blueskyk <1419293032@qq.com>
2021-01-16 17:41:07 +0800

    聊天输入框组件开发

---
+++ 糗事百科/components/user-chat/user-chat-bottom.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="user-chat-bottom u-f-ajc">
+		<input type="text" placeholder="文明发言" v-model="text" />
+		<view class="icon iconfont icon-fabu u-f-ajc" @tap="submit"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				text: ""
+			}
+		},
+		methods: {
+			submit() {
+				this.$emit("submit",this.text)
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.user-chat-bottom {
+		position: fixed;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		height: 120rpx;
+		padding: 0 20rpx;
+		background-color: #fff;
+		border-top: 1rpx solid #ccc;
+		input {
+			flex: 1;
+			margin-right: 20rpx;
+			padding: 10rpx 20rpx;
+			border-radius: 10rpx;
+			background-color: #f7f7f7;
+		}
+		.icon {
+			width: 80rpx;color: #000;
+			font-size: 45rpx;
+		}
+	}
+</style>
--- 糗事百科/pages/user-chat/user-chat.vue
+++ 糗事百科/pages/user-chat/user-chat.vue
@@ -1,22 +1,29 @@
 <template>
 	<view>
-		聊天页面
+		<!-- 输入框 -->
+		<user-chat-bottom @submit="submit"></user-chat-bottom>
 	</view>
 </template>
 
 <script>
+	import userChatBottom from "../../components/user-chat/user-chat-bottom.vue"
 	export default {
+		components: {
+			userChatBottom
+		},
 		data() {
 			return {
 				
 			}
 		},
 		methods: {
-			
+			submit(text) {
+				console.log(text)
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
