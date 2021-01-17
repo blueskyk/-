fc133d9a5faa449243ce3666bc6a13e3ead44abc
blueskyk <1419293032@qq.com>
2021-01-17 21:32:20 +0800

    用户聊天列表的封装

---
+++ 糗事百科/components/user-chat/user-chat-list.vue
@@ -0,0 +1,76 @@
+<template>
+	<!-- 聊天列表组件 -->
+	<view>
+		<view class="user-chat-time u-f-ajc">{{ item.settime }}</view>
+		<view class="user-chat-list u-f" :class="{ 'user-chat-me': item.isMe }">
+			<image v-if="!item.isMe" class="user-pic" :src="item.userPic" mode="widthFix" lazy-load></image>
+			<view class="user-chat-content">
+				<text v-if="item.type == 'text'">{{ item.content }}</text>
+				<image v-if="item.type == 'img'" :src="item.content" mode="widthFix"></image>
+			</view>
+			<image v-if="item.isMe" class="user-pic" :src="item.userPic" mode="widthFix" lazy-load></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			item: Object,
+			index: Number
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.user-chat-time {
+		padding: 50rpx 0;
+		font-size: 24rpx;
+		color: #a2a2a2;
+	}
+	.user-chat-list {
+		padding: 20rpx 0;
+		.user-pic {
+			width: 100rpx;
+			width: 100rpx;
+			border-radius: 50%;
+			flex-shrink: 0;
+		}
+		.user-chat-content {
+			position: relative;
+			background-color: #f4f4f4;
+			padding: 25rpx;
+			margin-left: 20rpx;
+			margin-right: 100rpx;
+			border-radius: 20rpx;
+			&::after {
+				position: absolute;
+				left: -30rpx;
+				right: 0;
+				top: 30rpx;
+				content: '';
+				width: 0;
+				height: 0;
+				border: 16rpx solid #f4f4f4;
+				border-color: transparent #f4f4f4 transparent transparent;
+			}
+		}
+	}
+	
+	.user-chat-me {
+		justify-content: flex-end;
+		.user-chat-content {
+			margin-right: 20rpx;
+			margin-left: 100rpx;
+			&::after {
+				left: auto;
+				right: -30rpx;
+				border-color: transparent transparent transparent #f4f4f4;
+			}
+			image {
+				max-width: 150rpx;
+				max-height: 200rpx;
+			}
+		}
+	}
+</style>
--- 糗事百科/pages/user-chat/user-chat.vue
+++ 糗事百科/pages/user-chat/user-chat.vue
@@ -1,27 +1,59 @@
 <template>
 	<view>
+		<!-- 聊天列表 -->
+		<block v-for="(item, index) in list" :key="index">
+			<user-chat-list :item="item" :index="index"></user-chat-list>
+		</block>
 		<!-- 输入框 -->
 		<user-chat-bottom @submit="submit"></user-chat-bottom>
 	</view>
 </template>
 
 <script>
-	import userChatBottom from "../../components/user-chat/user-chat-bottom.vue"
-	export default {
-		components: {
-			userChatBottom
+import time from '../../common/time.js';
+import userChatBottom from '../../components/user-chat/user-chat-bottom.vue';
+import userChatList from "../../components/user-chat/user-chat-list.vue"
+export default {
+	components: {
+		userChatBottom,
+		userChatList
+	},
+	data() {
+		return {
+			list: []
+		};
+	},
+	methods: {
+		submit(text) {
+			console.log(text);
 		},
-		data() {
-			return {
-				
+		getData() {
+			let arr = [
+				{
+					isMe: false,
+					userPic: '../../static/demo/userpic/6.jpg',
+					type: 'text',
+					content: '哈哈哈哈',
+					time: '1610800529'
+				},
+				{
+					isMe: true,
+					userPic: '../../static/demo/userpic/11.jpg',
+					type: 'img',
+					content: '../../static/demo/datapic/12.jpg',
+					time: '1610803370'
+				}
+			];
+			for(let i = 0; i < arr.length; i++) {
+				arr[i].settime = time.gettime.gettime(arr[i].time)
 			}
-		},
-		methods: {
-			submit(text) {
-				console.log(text)
-			}
+			this.list = arr; 
 		}
+	},
+	onLoad() {
+		this.getData();
 	}
+};
 </script>
 
 <style lang="scss">
