6ee0fe53be2d2696749ddcb08b7b78aa744776a3
blueskyk <1419293032@qq.com>
2021-01-21 19:47:17 +0800

    修改生日功能实现

--- 糗事百科/pages/change-userinfo/change-userinfo.vue
+++ 糗事百科/pages/change-userinfo/change-userinfo.vue
@@ -2,43 +2,45 @@
 	<view class="body">
 		<view class="change-info-list u-f-ac u-f-jsb">
 			<view class="list-left">头像</view>
-			<view class="u-f-ac list-right">
-				<image src="../../static/demo/userpic/6.jpg" mode="widthFix" lazy-load></image>
+			<view class="u-f-ac list-right" @tap="changeImg">
+				<image :src="userPic" mode="aspectFill" lazy-load></image>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="change-info-list u-f-ac u-f-jsb">
 			<view class="list-left">昵称</view>
 			<view class="u-f-ac list-right">
-				<view>昵称</view>
+				<input type="text" :placeholder="userName" />
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="change-info-list u-f-ac u-f-jsb">
 			<view class="list-left">性别</view>
-			<view class="u-f-ac list-right">
-				<view>男</view>
+			<view class="u-f-ac list-right" @tap="changeItem('sex')">
+				<view>{{ sex }}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="change-info-list u-f-ac u-f-jsb">
 			<view class="list-left">生日</view>
-			<view class="u-f-ac list-right">
-				<view>2000-11-07</view>
-				<view class="icon iconfont icon-bianji1"></view>
-			</view>
+			<picker mode="date" :value="birthday" :start="startDate" :end="endDate" @change="bindDateChange">
+				<view class="u-f-ac list-right">
+					<view>{{ birthday }}</view>
+					<view class="icon iconfont icon-bianji1"></view>
+				</view>
+			</picker>
 		</view>
 		<view class="change-info-list u-f-ac u-f-jsb">
 			<view class="list-left">情感</view>
-			<view class="u-f-ac list-right">
-				<view>未婚</view>
+			<view class="u-f-ac list-right" @tap="changeItem('marry')">
+				<view>{{ marry }}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="change-info-list u-f-ac u-f-jsb">
 			<view class="list-left">职业</view>
-			<view class="u-f-ac list-right">
-				<view>IT</view>
+			<view class="u-f-ac list-right" @tap="changeItem('job')">
+				<view>{{ job }}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
@@ -54,18 +56,92 @@
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
+let sex = ['不限', '男', '女'];
+let marry = ['保密', '未婚', '已婚'];
+let job = ['开发', '运维', '产品'];
+export default {
+	data() {
+		return {
+			userPic: '../../static/demo/userpic/6.jpg',
+			userName: '哈哈哈',
+			sex: '不限',
+			job: 'IT',
+			marry: '未婚',
+			birthday: '2000-11-07'
+		};
+	},
+	methods: {
+		submit() {},
+		// 修改用户头像
+		changeImg() {
+			uni.chooseImage({
+				count: 1,
+				sizeType: ['compressed'],
+				success: res => {
+					this.userPic = res.tempFilePaths;
+				}
+			});
 		},
-		methods: {
-			submit() {
-				
+		// 实现单项选择
+		changeItem(val) {
+			let arr = [];
+			switch (val) {
+				case 'sex':
+					arr = sex;
+					break;
+				case 'marry':
+					arr = marry;
+					break;
+				case 'job':
+					arr = job;
+					break;
 			}
+			uni.showActionSheet({
+				itemList: arr,
+				success: res => {
+					switch (val) {
+						case 'sex':
+							this.sex = arr[res.tapIndex];
+							break;
+						case 'marry':
+							this.marry = arr[res.tapIndex];
+							break;
+						case 'job':
+							this.job = arr[res.tapIndex];
+							break;
+					}
+				}
+			});
+		},
+		getDate(type) {
+			const date = new Date();
+			let year = date.getFullYear();
+			let month = date.getMonth() + 1;
+			let day = date.getDate();
+
+			if (type === 'start') {
+				year = year - 60;
+			} else if (type === 'end') {
+				year = year + 2;
+			}
+			month = month > 9 ? month : '0' + month;
+			day = day > 9 ? day : '0' + day;
+			return `${year}-${month}-${day}`;
+		},
+		// 修改生日
+		bindDateChange(e) {
+			this.birthday = e.target.value
+		}
+	},
+	computed: {
+		startDate() {
+			return this.getDate('start');
+		},
+		endDate() {
+			return this.getDate('end');
 		}
 	}
+};
 </script>
 
 <style lang="scss">
@@ -85,6 +161,9 @@
 		}
 		.icon {
 			margin-left: 20rpx;
+		}
+		input {
+			text-align: right;
 		}
 	}
 }
